b68c4f54bc85ffb7d5e111dfedd55b78332ae13e
Philipp Woelfel <woelfel@ucalgary.ca>
2018-09-26 18:19:33 -0600

    Updated script

--- README.md
+++ README.md
@@ -13,6 +13,34 @@ Runs `<command>`, if the XWindow system was idle for at least `<idle time>` *and
 
 Several reports can be obtained by using options `--warnings`, `--report`, or `--color-report`.
 
+## Configuration file:
+Each line is of one of following types:
+
+### Comment Lines
+Indicated by  leading `#`. Comment lines are ignored
+
+### Command specification lines
+Of the form
+
+```
+  <delta time> <idle time> <command>
+```
+
+where `delta time` and `idle time` must be valid time specifications.
+
+`<command>` will be executed, if it was not run successfully for `<delta time>`, and the X system has been idle for at least `<idle time>`
+
+### Variable configurations
+A line of the form `<variable>=<value>`
+This sets the variable to the given value.
+Currently, the following variables are recognized:
+
+* `WARNTIME`: Reports flag each command in the following command specification lines as *warning*, if the command was not run for the time specified.
+* `ERRTIME`: Reports flag each command in the following command specification lines as *error*, if the command was not run for the time specified.
+
+
+
 ## Requirements:
 * ansi-color: https://code.google.com/p/ansi-color/
 * xprintidle: https://github.com/g0hl1n/xprintidle (executable`xprintidle` must be in $PATH.
+* md5sum
--- smartstart.sh
+++ smartstart.sh
@@ -1,26 +1,52 @@
+
 #!/bin/bash
 #Needs ansi-color installed: https://code.google.com/p/ansi-color/
-
 
 SMSTDIR=$HOME/.smartstart
 SPOOLDIR=$SMSTDIR/spool
 LOGDIR="$SMSTDIR/log"
+DBASE="$SMSTDIR/dbase.csv"
+CONF="$SMSTDIR/smartstart.conf"
 
-WARNTHRESHHOLD=3
-ERRTHRESHHOLD=7
+RESETCOLOR="\e[0m"
+
+
+##################################################
+#These can be changed by the user:
+#For excape sequences see https://misc.flogisoft.com/bash/tip_colors_and_formatting
+user_variables="SUCCESSCOLOR ERRCOLOR WARNCOLOR WARNTIME ERRTIME"
+SUCCESSCOLOR="\e[48;5;28m"
+ERRCOLOR="\e[41m"
+WARNCOLOR="\e[43m"
+WARNTIME=3d
+ERRTIME=7d
+##################################################
 
 function print_usage {
  SNAME=`basename $0`
- echo
- echo "usage: $SNAME <options> <delta time> <idle time> <command>"
- echo "  where <delta time> and <idle time>"
- echo "  are specified as <integer>[m|h\d]."
- echo
- echo "Runs <command>, if the X system was idle <idletime> AND"
- echo "<command> was last run <delta time> ago"
- echo
- echo "To print warnings or report use --warnings --report or --color-report"
- exit 1
+ cat <<"EOF"
+usage: $SNAME [<options>] <command>
+
+options:
+[-c|--conf] <filename>: specify a configuration file (default: "$CONF").
+[-d|--dbase] <filename>": use <filename> for database (default: "$DBASE").
+
+EOF
+  exit 1
+}
+
+function report {
+ now=`date +%s`
+ shift 2
+ cmdname=`cat "$*"`
+ hash=`get_hash "$cmdname"`
+ SPOOLFILE="$SPOOLDIR"/"$hash"
+ modtime=`stat -c"%Y" $SPOOLFILE`
+ seconds=$((now-modtime))
+ minutes=$((seconds/60))
+ hours=$((minutes/60))
+ days=$((hours/24))
+ printf "%03dd %02dh %02dm ago:   $cmdname  (`basename $SPOOLFILE`)\n" ${days} $((hours%24)) $((minutes%60))
 }
 
 function print_report {
@@ -60,8 +86,8 @@ function do_print_warnings {
   while read line; do
     days=`echo $line | cut -c 1-3`
     col=""
-    test $days -ge $WARNTHRESHHOLD && col="$(color black yellow)"
-    test $days -ge $ERRTHRESHHOLD && col="$(color black magenta)"
+    test $days -ge $WARNTIME && col="$(color black yellow)"
+    test $days -ge $ERRTIME && col="$(color black magenta)"
     test "$col" != "" && echo "$col$line$(color nm)" && wct=$((wct+1))
   done
 }
@@ -74,11 +100,11 @@ function print_warnings {
   scripts=`print_report | wc -l`
   warnings=`do_print_warnings | wc -l`
   if [ $warnings -ge 1 ]; then
-    echo "${scripts} smartsart scripts were run in the last $WARNTHRESHHOLD days"
+    echo "${scripts} smartsart scripts were run in the last $WARNTIME days"
     echo "$(color black magenta)Warnings:$(color nm)"
     do_print_warnings
   else
-    echo "$(color black green)${scripts} smartsart scripts were run in the last $WARNTHRESHHOLD days$(color nm)"
+    echo "$(color black green)${scripts} smartsart scripts were run in the last $WARNTIME days$(color nm)"
   fi
   exit 0
 }
@@ -92,7 +118,7 @@ function string_to_minutes {
   local timestr=$1
 
   #check length of $timestr
-  test ${#timestr} -le 1 && echo "error: '$1' is not a valid time specification" && print_usage
+  test ${#timestr} -le 1 && return 1
 
   #get the correct time parameter:
   local spec=${timestr: -1:1}
@@ -106,8 +132,7 @@ function string_to_minutes {
     d)
       mins="$((int*60*24))";;
     *)
-      echo "error: '$1' is not a valid time specification"
-      print_usage
+      return 1
   esac
   eval $2="'$mins'"
 }
@@ -119,13 +144,92 @@ function get_hash {
  echo "$hash"
 }
 
-thiscmd="$*"
+function run_it {
+  #get parameters and Xidle time
+  delta="$1"
+  shift
+  idle="$1"
+  shift
+  to_exec="$*"
 
+  hash=`get_hash "$to_exec"`
+  d=`date "+%Y-%m-%d %H:%M:%S"`
+  echo -n "$to_exec ($hash). "
+  XidleMin=0
+  XidleMSec=`$XPRINTIDLE`
+  XidleSec=$((XidleMSec/1000))
+  test -z "$XidleSec" || XidleMin=$(($XidleSec/60))
+  #test "$delta" = "-h" -o "$idle" = "-h" -o "$to_exec" = "-h" && return 1
+
+  #f=`date "+%H%M%S"`
+  #echo "$f: $XidleSec sec = $XidleMin min"
+
+  #convert strings "delta" and "idle" into integers repr. minutes
+  string_to_minutes $delta delta_mins || return 1
+  string_to_minutes $idle idle_mins || return 1
+  echo -n "Required delta/idle: ${delta_mins}m/${idle_mins}m. "
+
+  SPOOLFILE="$SPOOLDIR"/"$hash"
+  ERRLOG="$LOGDIR"/"$hash.err"
+  OUTLOG="$LOGDIR"/"$hash.log"
+
+  #Check idle time
+  test $XidleMin -lt $idle_mins && echo "Insufficient idle time (${XidleMin}m)" && return 0
+
+  #Check delta time
+  if [ -e "$SPOOLFILE" ]; then
+    s=`find "$SPOOLFILE" -cmin "-$delta_mins" -print | tr -d ' '`
+    test "$s" != "" && echo "Insufficient delta time (${delta_mins}m)" && return 0
+  fi
+
+  # Create outputs and execute command
+  echo -n "Executing..."
+  echo -e "\n\n-----------------------------------------------------------------------------" | tee -a "$OUTLOG" "$ERRLOG" > /dev/null
+  echo "$d" | tee -a $OUTLOG "$ERRLOG" > /dev/null
+  echo "see '$ERRLOG' for stderr output" >> "$OUTLOG"
+  echo "see '$OUTLOG' for stdout output" >> "$OUTLOG"
+  echo -e "Stdout output of '$to_exec':" >> "$OUTLOG"
+  echo -e "Stderr output of '$to_exec':" >> "$ERRLOG"
+
+  ###########################################
+  # Executing the command
+  #bash -c "$to_exec 2>&1 >> $LOGFILE >& /dev/null"
+  bash -c "$to_exec > >(tee -a $OUTLOG) 2> >(tee -a $ERRLOG >&2)" >& /dev/null
+  ###########################################
+  EXITSTAT="$?"
+
+
+  echo -e "-----------------------------------------------------------------------------\nExit status: $EXITSTAT" | tee -a "$OUTLOG" "$ERRLOG" > /dev/null
+
+
+  # Check return status
+  if [ "$EXITSTAT" = "0" ]; then
+    echo -e "${SUCCESSCOLOR}Success.${RESETCOLOR}"
+    echo "$to_exec" >| "$SPOOLFILE"
+  else
+    echo -e "${ERRCOLOR}Failed.${RESETCOLOR}"
+  fi
+  echo -e "-----------------------------------------------------------------------------\n\n" | tee -a "$OUTLOG" "$ERRLOG" > /dev/null
+}
+
+#########################################
+# Error handling
+# See https://www.davidpashley.com/articles/writing-robust-shell-scripts/#id2382181
+set -e #stop on first error
+set -u #stop if using uninitialized variable
+#########################################
+
+
+
+#########################################
+# Check  executables
+#########################################
 #Check executables
-MD_SUM=/usr/bin/md5sum
 #XIDLETIME=$HOME/bin/xidletime.py
-XPRINTIDLE=/usr/bin/xprintidle
-for cmd in {$MD_SUM,$XPRINTIDLE}; do
+GETOPT=`which getopt`
+MD_SUM=`which md5sum`
+XPRINTIDLE=`which xprintidle`
+for cmd in {$MD_SUM,$XPRINTIDLE,$GETOPT}; do
   if [ ! -x "$cmd" ]; then
    print_usage
    echo "error: cannot find executable '$cmd'"
@@ -133,73 +237,118 @@ for cmd in {$MD_SUM,$XPRINTIDLE}; do
   fi
 done
 
+##########################################
+# Set default values for warn and err time
+##########################################
+string_to_minutes "$WARNTIME" WARNMIN
+string_to_minutes "$ERRTIME" ERRMIN
+
+
+#########################################
+# Parsing parameters
+#########################################
+set -- $($GETOPT --unquoted --options c:d:hr --longoptions conf:,dbase:,help,report -- "$@")
+echo checking options "$*"
+
+# Checking options
+while (($#)); do
+  case "$1" in
+    -d|--dbase)
+      DBASE="$2"
+      shift 2
+      ;;
+    -c|--conf)
+        CONF="$2"
+        shift 2
+        ;;
+    -h|--help)
+        shift
+        print_usage
+        ;;
+    -r|--report)
+        shift
+        REPORT=1
+        ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      print_usage
+      ;;
+  esac
+done
+
+test -z ${1+x} || print_usage
+
+#########################################
+# Checking files
+#########################################
+for d in {$SPOOLDIR,$LOGDIR}; do
+  mkdir -p $d
+  if [ ! -d $SPOOLDIR ]; then
+    echo "error: configuration file '$CONF'" not found
+    exit 1
+  fi
+done
+
+if [ ! -r "$CONF" ]; then
+  echo "error: configuration file '$CONF'" not found
+  exit 1
+fi
+if [ ! -r "$DBASE" ]; then
+  echo "warning: database '$DBASE' not found. Creating."
+  touch "$DBASE" > /dev/null || echo "error: cannot create '$DBASE'" && exit 1
+fi
+
+#########################################
+# Parsing the configuration file
+#########################################
+#while IFS='' read -r line || [[ -n "$line" ]]; do
+lineno=0
+set -e
+while read -r line; do
+    ((lineno+=1))
+    #note: $line has leading and trailing white space removed!
+    set -- "$line"
+
+    #Test for comment and empty lines
+    test -z "$line" && continue
+    set +e
+    comment=`echo "$line" | grep -x "#.*$"`
+    set -e
+    test -z "$comment" || continue
+
+    #Test for variable settings:
+    set +e
+    var=""
+    for v in ${user_variables}; do
+      var=$var`echo "$line" | grep -x "$v=.*$"`
+    done
+    set -e
+
+    if [ -z "$var" ]; then
+        #If we're note setting a variable, then either report or run smartstart script:
+        if [ "$REPORT" = "1" ]; then
+          report $*
+        else
+          run_it $* || echo "error in line $lineno"
+        fi
+    else
+      #set the variable and check the syntax
+      echo "setting $var"
+      eval $var
+      string_to_minutes "$WARNTIME" WARNMIN || echo "error (line $lineno): invalid time specification"
+      string_to_minutes "$ERRTIME" ERRMIN || echo "error (line $lineno): invalid time specification"
+    fi
+done < "$CONF"
+
+exit
+#########################################
+
+thiscmd="$*"
+
 test "$1" = "--help" && print_usage
 test "$1" = "--report" && print_report && exit 0
 test "$1" = "--color-report" && print_color_report
 test "$1" = "--warnings" && print_warnings
-
-# Make sure smartstart directories exist:
-mkdir -p $SPOOLDIR $LOGDIR
-
-#get parameters and Xidle time
-delta="$1"
-shift
-idle="$1"
-shift
-to_exec="$*"
-hash=`get_hash "$to_exec"`
-d=`date "+%Y-%m-%d %H:%M:%S"`
-echo -n "$d; $to_exec; $hash; "
-XidleMin=0
-XidleMSec=`$XPRINTIDLE`
-XidleSec=$((XidleMSec/1000))
-
-test -z "$XidleSec" || XidleMin=$(($XidleSec/60))
-test "$delta" = "-h" -o "$idle" = "-h" -o "$to_exec" = "-h" && print_usage
-
-f=`date "+%H%M%S"`
-echo "$f: $XidleSec sec = $XidleMin min"
-
-
-#convert strings "delta" and "idle" into integers repr. minutes
-string_to_minutes $delta delta_mins
-string_to_minutes $idle idle_mins
-echo -n "required delta/idle time: ${delta_mins}m/${idle_mins}m. "
-
-SPOOLFILE="$SPOOLDIR"/"$hash"
-LOGFILE="$LOGDIR"/"$hash.log"
-
-test -d "$SPOOLDIR" || mkdir "$SPOOLDIR"
-if [ ! -d "$SPOOLDIR" ]; then
-  echo "error: cannot create $SPOOLDIR"
-  exit
-fi
-
-test -d "$LOGDIR" || mkdir "$LOGDIR"
-if [ ! -d "$LOGDIR" ]; then
-  echo "error: cannot create $LOGDIR"
-  exit
-fi
-
-
-#Check idle time
-test $XidleMin -lt $idle_mins && echo "insufficient idle time" && exit
-#Check delta time
-s=`find "$SPOOLFILE" -cmin "-$delta_mins" -print | tr -d ' '`
-test "$s" != "" && echo "insufficient delta time" && exit
-
-echo "executing '$to_exec'"
-echo -e "\n\n-----------------------------------------------------------------------------" >> "$LOGFILE"
-echo "$d" >> "$LOGFILE"
-echo "$0 $thiscmd" >> "$LOGFILE"
-echo >> "$LOGFILE"
-($to_exec 2>&1) >> "$LOGFILE"
-
-if [ "$?" = "0" ]; then
-  echo "succeeded" >> "$LOGFILE"
-  echo "execution of '$to_exec' succeeded."
-  echo "$to_exec" >| "$SPOOLFILE"
-else
-  echo "execution of '$to_exec' failed."
-  echo "failed" >> "$LOGFILE"
-fi
